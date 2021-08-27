ae1b7c66cb24a50342286bf9547364d80a1c823f
bsh6463 <bsh9510@gmail.com>
2021-08-27 22:43:53 +0900

    210827 Domain model

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,29 +1,47 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "userName", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String userName;
 
-    protected Member() {
-    }
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String userName) {
         this.userName = userName;
     }
 
+    public Member(String userName, int age, Team team) {
+        this.userName = userName;
+        this.age = age;
+        if(team!=null) {
+            this.changeTeam(team);
+        }
+    }
+
     public void changeUserName(String userName){
         this.userName = userName;
     }
+
+    public void changeTeam(Team team){
+        this.setTeam(team);
+        team.getMembers().add(this);
+    }
+
 }
